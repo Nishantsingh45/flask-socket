fd5f0a15a9ea39ab215131d3a05ef70b4c150855
Nishant Singh <68099982+Nishantsingh45@users.noreply.github.com>
2023-10-23 11:56:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,68 @@
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit, join_room
+from qa_service import query_document
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-@app.route('/')
-def index():
-  return render_template('chat.html')
+# Store user and admin rooms
+user_room = "user_room"
+admin_room = "admin_room"
 
-@socketio.on('message')
+# Initialize bot_enabled flag
+bot_enabled = False
+
+# Dictionary to store user messages
+user_messages = {}
+
+@app.route("/user_interface")
+def user_interface():
+    return render_template("user_interface.html")
+
+@app.route("/admin_interface")
+def admin_interface():
+    return render_template("admin_interface.html", users=list(user_messages.keys()))
+
+@app.route("/admin_interface/<username>")
+def admin_with_user(username):
+    if username not in user_messages:
+        user_messages[username] = []
+    return render_template("admin_interface_with_user.html", username=username, messages=user_messages[username])
+
+@socketio.on("join_room")
+def join_user_room():
+    join_room(user_room)
+
+@socketio.on("message")
 def handle_message(data):
-  socketio.emit('message', data, broadcast=True)
+    if bot_enabled:
+        query = data["message"]
+        bot_response = query_document(query)  # Assuming query_document function is available
+        emit("message", {"user": "Admin", "message": bot_response}, room=user_room)
+    else:
+        emit("message", data, room=user_room, broadcast=True)
 
-if __name__ == '__main__':
-  socketio.run(app)
+@socketio.on("user_send_message")
+def handle_user_message(data):
+    username = data["user"]
+    message = data["message"]
+    if username not in user_messages:
+        user_messages[username] = []
+    user_messages[username].append({"user": username, "message": message})
+    emit("message", data, room=user_room)
+
+@socketio.on("admin_join_room")
+def join_admin_room():
+    join_room(admin_room)
+
+@socketio.on("admin_send_message")
+def handle_admin_send_message(data):
+    emit("message", data, room=user_room)
+
+@socketio.on("toggle_bot")
+def toggle_bot_reply():
+    global bot_enabled
+    bot_enabled = not bot_enabled
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
